d18fc4e2efe7983e02c8a37c11a027379d01b098
Tito Carlos <titocarlos080@gmail.com>
2024-06-14 10:28:01 -0400

    corrigiendo errores

--- models/comunicados.py
+++ models/comunicados.py
@@ -1,6 +1,7 @@
 from random import randint
+
+import requests
 from odoo import api, fields, models
-from odoo.addons.de_school.controllers.notificacion_controller import NotificacionController # type: ignore
 
 class Comunicados(models.Model):
     _name = 'oe.school.comunicados'
@@ -14,5 +15,38 @@ class Comunicados(models.Model):
         new_record = super(Comunicados, self).create(vals)
         print("data de impresion")
         print(vals)
-        NotificacionController.enviarNotificacionComunicado(vals)
+        self.enviarNotificacionComunicado(self,vals)
         return new_record
+    
+    def send_notification(self,device_token, title, body):
+        print("Clave kkkkkkkksd sdd  ==========",device_token)
+        server_key = 'AAAAM-ejTlM:APA91bEqdQRAjd_2_qXK23M9c7CbbDf7SVpjas789C9xMQXtEkUNSHeT4Gj9t3nTOG_Tk6JK9C4qDD1n1r0oZeRtI9otcoi-OhPJDsw93LypxhQpHRvi9ZG9eS_WmT_fHagLHSSZubtF'
+        url = 'https://fcm.googleapis.com/fcm/send'
+        headers = {
+            'Authorization': 'key=' + server_key,
+            'Content-Type': 'application/json',
+        }
+        data = {
+            'to': device_token,
+            'notification': {
+                'title': title,
+                'body': body,
+            },
+        }
+        response = requests.post(url, headers=headers, json=data)
+        return response.status_code
+
+
+    def enviarNotificacionComunicado(seft,comunicado):
+        print("este es el comunicado " ,comunicado)
+        
+        name = comunicado.get('name')
+        descripcion = comunicado.get('descripcion')
+        
+        Padres = requests.request.env['oe.school.padre'].sudo().search([])
+        
+        for padre in Padres:
+            mensaje = f"comunicado:  {name}  contexto: {descripcion}"
+            print("este es el padre y su key " ,padre.keynotificaciones)
+            seft.send_notification(seft,padre.keynotificaciones, 'Nuevos Comunicados', mensaje)
+        return
